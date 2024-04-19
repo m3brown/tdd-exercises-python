d7f862589987e5e7232a0c4efb566565cfb313d7
Mike Brown <mike.brown@excella.com>
2024-04-19 12:42:25 -0400

    Recommend tweaks to fixtures tests

--- fixtures/test_fixtures_multiple.py
+++ fixtures/test_fixtures_multiple.py
@@ -1,19 +1,31 @@
+# Use the -s flag to view STDOUT
+# pytest -s test_fixtures_multiple.py
 import pytest
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture()
 def foo():
     return "FOO"
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture()
 def bar(foo):
-    return "BAR"
+    return foo+"BAR"
 
 
 @pytest.fixture()
 def baz(foo, bar):
-    return "BAZ"
+    return foo+bar+"BAZ"
+
+
+@pytest.fixture(autouse=True)
+def auto():
+    print()
+    print("AUTO")
+
+
+def test_auto():
+    pass
 
 
 def test_multi(foo, bar, baz):
--- fixtures/test_fixtures_one.py
+++ fixtures/test_fixtures_one.py
@@ -14,7 +14,9 @@ def machine_with_one_dollar():
 def test_release_change_when_x_expect_y(machine_with_one_dollar):
     # Arrange
     machine_with_one_dollar.release_change()
+
     # Act
     actual = machine_with_one_dollar.change_return
+
     # Assert
     assert actual == 5
